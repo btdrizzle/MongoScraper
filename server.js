27cbc0a0a671777470776b765eca69c40e675145
btdrizzle <briandrake05@gmail.com>
2018-12-22 13:43:01 -0600

    set up complete

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const logger = reguire("morgan");
+const mongoose = require("mongoose");
+
+require("handlebars");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(expres.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+const exphbs = require("express-handlebars");
+app.engine("hbs", exphbs({
+	defaultLayout: "main",
+	extname: ".hbs"
+}));
+app.set("view engine", "hbs");
+
+require("./routes/routes.js")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
